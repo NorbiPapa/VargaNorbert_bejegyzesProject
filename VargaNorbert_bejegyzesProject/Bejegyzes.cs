2f0cf244a25ce65cf2b4624e98f4d4e4b6f624a8
Norbert Varga <norbika0210@gmail.com>
2023-09-26 08:28:41 +0200

    1. Feladat b részfeladat

---
+++ VargaNorbert_bejegyzesProject/Bejegyzes.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VargaNorbert_bejegyzesProject
+{
+    internal class Bejegyzes
+    {
+        private string szerzo;
+        private string tartalom;
+        private int like;
+        new DateTime letrejott;
+        new DateTime szerkeztve;
+
+        public Bejegyzes(string szerzo, string tartalom)
+        {
+            this.szerzo = szerzo;
+            this.tartalom = tartalom;
+            this.like = 0;
+            this.letrejott = DateTime.Today;
+            this.szerkeztve = DateTime.Today;
+        }
+
+        public string Szerzo { get => szerzo; }
+        public string Tartalom { get => tartalom; set => tartalom = value; }
+        public int Like { get => like;}
+        public DateTime Letrejott { get => letrejott;}
+        public DateTime Szerkeztve { get => szerkeztve;}
+
+        public void Likeok() 
+        {
+            like++;
+        }
+
+    }
+}
